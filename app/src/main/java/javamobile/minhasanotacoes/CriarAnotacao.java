4d88799dd83d528b4add17ca0ff5fd167bf366d6
Matheus Sanches <matheussanchesleme@gmail.com>
2020-01-12 13:37:40 -0300

    Restante dos arquivos da primeira versão do app adicionados.

---
+++ app/src/main/java/javamobile/minhasanotacoes/CriarAnotacao.java
@@ -0,0 +1,44 @@
+package javamobile.minhasanotacoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import javamobile.minhasanotacoes.bancodedados.BancoDeDados;
+
+public class CriarAnotacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_anotacao);
+    }
+
+    public void voltar (View v){
+        Intent startNewActivity = new Intent(this, TelaInicial.class);
+        startActivity(startNewActivity);
+    }
+
+    public void criarAnotacao(View v){
+        BancoDeDados bancoDeDados = new BancoDeDados(getBaseContext());
+        EditText titulo = (EditText) findViewById(R.id.campoTitulo);
+        EditText conteudo = (EditText) findViewById(R.id.campoConteudo);
+
+        if (!(titulo.getText().toString().trim().isEmpty())) {
+            boolean resultado = bancoDeDados.criarAnotacao(titulo.getText().toString(), conteudo.getText().toString());
+
+            if (resultado) {
+                Toast.makeText(getApplicationContext(), "Anotação criada com sucesso!", Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Infelizmente ocorreu um erro, tente novamente.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        voltar(v);
+
+    }
+}
